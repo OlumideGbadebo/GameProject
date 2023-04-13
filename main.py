560e76d40e2fb60f6db9dee880640c1857979b5b
sam60420 <git@github.com:sam60420>
2023-04-13 06:53:11 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((1000, 800))
+
+backgroundImg = pygame.image.load("background.png")
+
+
+
+
+
+
+
+
+class Rectangle:
+    player_length = 2.5
+    player_width = 3.6
+    player_height = 6.1
+
+class Hero:
+    hero_1 = 'Lu'
+
+
+
+
+
+
+    pygame.display.update()
